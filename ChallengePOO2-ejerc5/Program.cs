35650fb7cbdeb70d4ff2eb704104634959b6d91e
sergio351 <velazquezsergio714@gmail.com>
2022-05-08 21:49:37 -0300

    primercommit

---
+++ ChallengePOO2-ejerc5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+public class ClaseAbstracta
+{
+    public static void Main(string[] args)
+    {
+        Perro perro = new Perro();
+        Console.WriteLine("ingrese nombre: ");
+        perro.SetNombre(Console.ReadLine());
+        Console.WriteLine(perro.GetNombre());
+        perro.Comer();
+    }
+
+    public class Perro : Animal
+    {
+        public override void Comer()
+        {
+            Console.WriteLine("esta comiendo");
+        }
+    }
+
+    public abstract class Animal
+    {
+        private string Nombre;
+
+        public void SetNombre(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public string GetNombre()
+        {
+            return Nombre;
+        }
+
+        public abstract void Comer();
+    }
+}
